fac267f0e80374a9bb540651b7619e53b93b2d72
arunjain27 <2003010048@ipec.org.in>
2023-07-06 11:00:25 +0530

    all the branch file is committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import Navbar from './components/Navbar';
+import News from './components/News';
+// import "./components/app.css"
+// import Home from './components/Home';
+// import About from './components/About';
+// import {
+//   BrowserRouter,  
+//   Routes,
+//    Route} from "react-router-dom";
+ import LoadingBar from 'react-top-loading-bar'
+ import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
+import { useState } from 'react';
+const App =()=>{
+     var pageSize = 8;
+    // const [progress, setProgress] = useState(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [progress,setprogress]=useState(0);
 
+return (
+<div>
+ 
+       <LoadingBar
+        color='#f11946'
+      height={3}
+       progress={progress} 
+        /> 
+
+
+
+             <BrowserRouter>  
+             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsBook</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      
+                         
+                          
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                           
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+	
+          		<Routes>
+            
+          
+          
+          
+                
+             
+         
+             <Route exact path="/" element={<News setprogress={setprogress}key="general" pageSize={pageSize} country="in" category="general"/>}/>
+           
+             <Route exact path="/business" element={<News setprogress={setprogress} key="business" pageSize={pageSize} country="in" category="business"/>}/> 
+           
+             <Route exact path="/entertainment" element={<News setprogress={setprogress} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/> 
+             <Route exact path="/general" element={<News setprogress={setprogress}  key="general" pageSize={pageSize} country="in" category="general"/>}/> 
+             <Route exact path="/health" element={<News setprogress={setprogress} key="health" pageSize={pageSize} country="in" category="health"/>}/> 
+             <Route exact path="/science" element={<News setprogress={setprogress}key="science" pageSize={pageSize} country="in" category="science"/>}/> 
+             <Route exact path="/sports" element={<News  setprogress={setprogress} key="sports" pageSize={pageSize} country="in" category="sports"/>}/> 
+             <Route exact path="/technology" element={<News  setprogress={setprogress} key="technology" pageSize={pageSize} country="in" category="technology"/>}/> 
+           </Routes>
+        
+           
+          
+          
+          
+          
+          
+          
+          
+          
+          	
+        	</BrowserRouter> 
+ 
+
+
+
+      </div>
+    ) 
+
+  
+} 
 export default App;
---
+++ src/components/News.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import Item from './Item'
+import PropTypes from 'prop-types'
+import "./app.css"
+const News=(props)=>{
+ 
+   const[articles,setarticles]=useState([]);
+   const[loading,setloading]=useState(true);
+   const[page,setpage]=useState(1);
+
+   const[totalresults,settotalresults]=useState(0);
+
+
+const updatenews = async ()=>
+{
+     
+    props.setprogress(10);
+    let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=d5891c5b3a5b4a4d87afc7a0d00465fd&page=${page}&pageSize=${props.pageSize}`
+    
+    props.setprogress(30);
+    setloading(true);
+    let data = await fetch(url);
+    props.setprogress(50);
+    let parsedata= await data.json();
+    props.setprogress(70);
+    setarticles(parsedata.articles);
+   
+    settotalresults(parsedata.totalresults);
+    setloading(false);
+    props.setprogress(100);
+    
+
+}
+
+useEffect(()=>{
+  updatenews();
+
+},[])
+
+ 
+const handleprevclick = async ()=>{
+console.log("prev");
+setpage(page-1);
+updatenews();
+ 
+}
+const handlenextclick = async ()=>{
+    console.log("next");
+
+        setpage(page+1)
+       updatenews();
+      }
+  
+ 
+        return (
+            <>
+  <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' ,color:"#1f456e" }}>{props.category}</h1>
+            
+
+
+           {loading &&  <div>
+            
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq3sWjCt0zs5sZ554c2_14J3przOA-llIp_Q&usqp=CAU" alt="" srcset="" />
+            </div>}
+
+                    <div className="container">
+                 
+
+                   <div className="row">
+                       {
+                        articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <Item title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
+                    </div>
+                 
+                   <div className='btn'>
+    
+
+                     <button  className='buttonprev' disabled={page<=1} type="button"onClick={handleprevclick} >prev</button>
+                     <button   className='buttonprev' disabled={page+1>Math.ceil(totalresults/20)} onClick={handlenextclick}>next</button>
+
+                   </div>
+
+                 
+                    </div> 
+                         
+
+            </>
+        )
+    
+}
+
+
+
+News.defaultProps = {  
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+}
+
+export default News  
